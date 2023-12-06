688b6929a1deb03430b351f2c56a4e20bbad588c
rajesh-k-iitm <153080008+rajesh-k-iitm@users.noreply.github.com>
2023-12-06 16:52:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,50 @@
-print("Hello world! This is RK")
+# Edited by Rajesh K, IITM, TDS GA8 
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello RK",
+        page_icon="👋",
+    )
+
+    st.write("# TDS GA8 - Welcome to Streamlit RK's page! 👋")
+
+    st.sidebar.success("Success!! Select a demo above.")
+
+    st.markdown(
+        """
+        IIT Madras Tools for Data Science Graded Assignment 8
+        6th December 2023
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+        **👈 Select a demo from the sidebar** to see some examples
+        of what Streamlit can do!
+      
+        ### See more complex demos
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
